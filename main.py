5bde2cd6d1018c0a2a8ce56d4f4bbdedeea0b204
denermauesn <denermaues@gmail.com>
2019-11-07 19:34:00 +0000

    l

---
+++ main.py
@@ -0,0 +1,82 @@
+import random
+import pandas
+
+class Jogo:
+    def __init__(self, dezenas, total):
+        self._dezenas    = None
+        self._resultado  = None
+        self._total      = None
+        self._jogos      = None
+
+        self.set_dezenas(dezenas)
+        self.set_total(total)
+
+
+
+    def get_dezenas(self):
+        return self._dezenas
+
+    def get_total(self):
+        return self._total
+
+    def set_dezenas(self, dezenas):
+        if dezenas not in [6,7,8,9,10]:
+            print("dezena precisa estar entre  6,7,8,9,10")
+
+        else:
+            self._dezenas = dezenas
+
+    def set_total(self, total):
+        self._total = total
+
+    def __list_dezenas(self):
+        dezenas = []
+        while len(dezenas) < self._dezenas:
+            rnd = random.randint(0,60)
+            if rnd not in dezenas:
+                dezenas.append(rnd)
+
+        return sorted(dezenas)
+
+    def add_jogos(self):
+        jogos_list = []
+        for i in range(self._total):
+            jogos_list.append(self.__list_dezenas())
+
+        self._jogos = jogos_list
+
+    def sorteio(self):
+        dezenas = []
+        while len(dezenas) < 6:
+            rnd = random.randint(0,60)
+            if rnd not in dezenas:
+                dezenas.append(rnd)
+
+        self._resultado = sorted(dezenas)
+
+    def print_jogos(self):
+        return self._jogos
+
+    def resultado_final(self):
+        tabela_final = []
+        for  jogo in self._jogos:
+            ac = 0
+            for dez in jogo:
+                if dez in self._resultado:
+                    ac += 1
+
+            tabela_final.append([jogo, ac])
+
+        data = pandas.DataFrame(tabela_final, columns=['Jogo', 'Acertos'])
+        html = data.to_html()
+        return html
+
+
+j = Jogo(10,5)
+
+j.add_jogos()
+j.sorteio()
+html = j.resultado_final()
+print(html)
+with open('tabela.html',  'w') as tabela:
+    tabela.write(html)
